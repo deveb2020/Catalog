8dc389fef637c5d58f981373df20fe04520f624c
egzon <egzonberisha232@gmail.com>
2022-01-05 20:08:15 +0100

    Dependecies injection, DTOs

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,7 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Catalog.Repositories;
 using System.Collections.Generic;
 using Catalog.Entities;
+using System.Linq;
+using Catalog.Dtos;
 
 namespace Catalog.Controllers
 {
@@ -9,18 +12,33 @@ namespace Catalog.Controllers
 		[Route("Items")]
     public class ItemsController : ControllerBase
     {
-      private readonly InMemItemsRepository repository;
+      private readonly IItemsRepository repository;
 
-			public ItemsController()
+			public ItemsController(IItemsRepository repository)
 			{
-				repository = new InMemItemsRepository();
+				this.repository = repository;
 			}
 
+			// GET /items
 			[HttpGet]
-			public IEnumerable<Item> GetItems()
+			public IEnumerable<ItemDto> GetItems()
 			{	
-				var items = repository.GetItems();
+				var items = repository.GetItems().Select( item => item.AsDto());
+
 				return items;
+			}
+
+			// GET /items/{id}
+			[HttpGet("{id}")]
+			public ActionResult<ItemDto> GetItem(Guid id)
+			{
+				var item = repository.GetItem(id);
+				
+				if ( item is null ) 
+				{
+					return NotFound();
+				}
+				return item.AsDto();
 			}
     }
 }
